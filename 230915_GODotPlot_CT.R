e923d2197113d052f5abd8ede63a0192495969c3
LineWulff <lw@himalaya.dk>
2023-11-08 08:20:12 -0700

    New input from Carolyn

--- 230915_GODotPlot_CT.R
+++ 230915_GODotPlot_CT.R
@@ -14,17 +14,19 @@ setwd(dir)
 dato <- str_sub(str_replace_all(Sys.Date(),"-","_"), 3, -1)
 
 #### Read in data from CT ####
-GO_df <- read.csv(paste(dir,"inputdata","Metabolismpathways_quicklook_sh2.csv", sep="/"))
-GO_df <- GO_df[1:20,c("Pathway","Strength","Ratio","p.adj")]
+GO_df <- read.csv(paste(dir,"inputdata","20231107_GOdata.csv", sep="/")) # Metabolismpathways_quicklook_sh2
+#GO_df <- GO_df[1:20,c("Pathway","Strength","Ratio","p.adj")]
+colnames(GO_df) <- c("term_ID","Pathway","Ratio","Strength","FDR")
+GO_df <- GO_df[1:20,c("Pathway","Strength","Ratio","FDR")]
 head(GO_df)
 
 GO_df$Pathway <- factor(GO_df$Pathway, levels = c(GO_df[order(GO_df$Strength),]$Pathway))
 
 
 #### Plot data ####
-pdf(paste(dir,"/output/",dato,"GO_DotPlot_sh2.pdf",sep = ""), height = 6,width = 8)
+pdf(paste(dir,"/output/",dato,"GO_DotPlot_v3.pdf",sep = ""), height = 6,width = 12)
 ggplot(data = GO_df, aes(x = Strength, y = Pathway, 
-                        color = Ratio, size = -log10(p.adj))) + 
+                        color = Ratio, size = -log10(FDR))) + 
   geom_point() +
   scale_color_viridis_c()+
   theme_bw() + 
